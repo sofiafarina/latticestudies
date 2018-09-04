10e0ecc3efec6d5352c34d9134c128baf7d93d85
sofiafarina <sofia.farina2@studio.unibo.it>
2018-09-04 07:52:58 +0200

    Update GA.py

--- GA.py
+++ GA.py
@@ -11,7 +11,7 @@ from numpy import linalg as LA
 from mpl_toolkits.mplot3d import Axes3D
 from scipy import stats
 
-
+np.random.seed(123)
 
 # creating lattice 
 N = 20 # dimensioni
@@ -19,12 +19,13 @@ F = 0 # numero di nodi rimossi
 L = 0 # numero di link rimossi
 
 n_population = 100
-elit_rate = 0.15
+elit_rate = 0.10
 ELIT = int(n_population * elit_rate)
 HALF = int(n_population / 2)
-max_iter = 30
-MUTATION_RATE = 0.30
-precision = 1e-4
+max_iter = 180
+MUTATION_RATE = 0.5
+precision = 1e-6
+m = 150
 
 # to remove random link and nodes
 def remove_random_nodes(A,N):
@@ -35,7 +36,6 @@ def remove_random_links(A,E):
     rs = random.sample(A.edges, E)
     A.remove_edges_from(rs)
     
-
 def kabsch(pt_true, pt_guessed): 
     # Translation 
     pt_true -= pt_true.mean(axis = 0)
@@ -61,9 +61,6 @@ def kabsch(pt_true, pt_guessed):
 
 
 def get_coord(lap):
-    #print(type(lap))
-    #print(lap[0][0].dtype)
-    #print(len(lap))
     eigw, eigv = LA.eig(lap)
     vecs = eigv[:,1:3]
     guess_coords = pd.DataFrame(data=vecs, columns=["x", "y"])
@@ -72,59 +69,42 @@ def get_coord(lap):
 
 def random_population(dim = N*N, n_pop = n_population):
     # creo n_pop vettori di lunghezza N*N di masse random 
- 
     #mu, sigma = 1.6, 0.6
-    low, high = 0.5,1.5
-    
+    low, high = 0.0,1.0
     #diag = np.random.normal(mu,sigma,size=[n_pop,N*N])
     diag =  np.random.uniform(low,high,size=[n_pop,N*N])
-    
+    #diag = np.random.binomial(40,0.5,size=[n_pop,N*N])*0.1
+    #diag = np.random.binomial(2,0.4,[n_pop,N*N])
     return diag 
 
 def crossover(vec_a, vec_b):
-    #print("crossin", len(vec_a), len(vec_b))
     rd = np.random.randint(1,N*N)
-    
     new_a = vec_a[0:rd]
     new_b = vec_b[rd:N*N]
     new = np.concatenate((new_a,new_b))
-    #print("crossin", len(new))
-        
+
     return new
 
 def mutate(vec):
-
-    rd = np.random.randint(1,N*N)
-    rdd = np.random.randint(1,N*N)
-    rddd = np.random.randint(1,N*N)
-    mu, sigma = 1.3, 0.5
-    new = np.random.normal(mu, sigma, 1)
-    neww = np.random.normal(mu, sigma, 1)
-    newww = np.random.normal(mu, sigma, 1)
-    #low, high = 0.5,2.5
-    #new = np.random.uniform(low, high, 1)
-    #neww = np.random.uniform(low, high, 1)
-    vec[rd] = new
-    vec[rdd] = neww
-    vec[rddd] = newww
+    mu, sigma = 0.7, 0.1 # 0.7, 0.5 ||  2.1, 0.5  
+    #low, high = 0.0,1.0
+    for i in range(m):
+        rd = np.random.randint(1,N*N)
+        #new = np.random.uniform(low, high, 1)
+        new = np.random.normal(mu, sigma, 1)
+        vec[rd] = new
     
     return vec 
 
 def new_generation(idx, old_generation, elit_rate = ELIT, mutation_rate = MUTATION_RATE, half = HALF):
     n_a = [old_generation[idx[i]] for i in range(0,ELIT)]
-    #print("na",n_a)
-    n_b = [crossover(old_generation[np.random.randint(0,n_population/2.)], old_generation[np.random.randint(n_population/2., n_population)]) for j in range(ELIT, n_population)] 
-    #for j in range(0,int(n_population-ELIT)):
-        #print("nb",len(n_b[j]))
-   
+    n_b = [crossover(old_generation[idx[np.random.randint(0,n_population/2.)]], old_generation[idx[np.random.randint(n_population/2., n_population)]]) for j in range(ELIT, n_population)] 
+
     new_gen = np.concatenate((n_a,n_b))
-    
-    #for n in range(n_population):
-        #print("sizep", len(new_gen[n]))
+
     for j in range(1,n_population):
         if np.random.rand() < mutation_rate:
             new_gen[j] = mutate(new_gen[j])
-    #print("new gen fatta", len(new_gen))
     return new_gen
 
 
@@ -143,18 +123,17 @@ def fitness(lap, t_coord):
                                                     for comb in combo ]
                                                     )])
     
-    return np.sqrt( np.sum( np.sum( (t_coord - gcoord[permutation])**2, axis=1) )/N )
-    
+    return np.sqrt( np.sum( np.sum( (t_coord - gcoord[permutation])**2, axis=1) )/(N**2) )
+    #return np.std(np.sum( (t_coord - gcoord[permutation])**2, axis=1) )
 
 
 def GA(real_coords, laplacian, max_iter = max_iter):
     
     population = random_population()
-    #print("random pop:", population)
+    fittini = []
     
     for generation in range(max_iter): 
         print("generation : %d"%generation)
-        #print("pop[0]:", population[0])
         fit = []
         for individual in population:
             d = np.diagflat(individual)
@@ -167,13 +146,14 @@ def GA(real_coords, laplacian, max_iter = max_iter):
         best_id = idx[0]   
         print("best id",best_id)
         print("best fit",fit[best_id])
+        fittini.append(fit[best_id])
         
         if(fit[best_id] <= precision):
             break
         else:
             population = new_generation(idx, population)
-            #print("fit new gen:",[fitness(np.dot(np.diagflat(individual),laplacian), real_coords) for individual in population])
-    
+    print("fittini:", fittini)
+    plt.scatter(np.arange(0,max_iter), fittini)
     return population[0]
 
 
@@ -234,16 +214,20 @@ print("tot_dist is")
 print(tot_dist)
 RMSD = tot_dist
 print("RMSD is")
-print(np.sqrt( np.sum( np.sum( (tcoord - gcoord[permutation])**2, axis=1) )/N ))
+print(np.sqrt( np.sum( np.sum( (tcoord - gcoord[permutation])**2, axis=1) )/(N**2) ))
 #%%
 # genetic algorithm
-lap_genetic = GA(tcoord, L) 
+tc = pd.DataFrame(np.asarray(R.nodes), columns=["x", "y"], dtype = float)
+lap_genetic = GA(tc, L) 
+
+#%%
 masses = np.diag(LA.inv(np.diagflat(lap_genetic))) # così tiro fuori i valori delle masse 
+#masses = lap_genetic
 print("masses",masses)
 
 
-#%%
-# plots
+
+
 nuovolaplaciano = np.dot(np.diagflat(lap_genetic), L.toarray())
 print(nuovolaplaciano)
 gc = get_coord(nuovolaplaciano)
@@ -265,46 +249,56 @@ permutation = list(combo[ np.argmax( [ sum([ scipy.stats.pearsonr(x=tcoord[true]
                                                     for comb in combo ]
                                                     )])
 
-
-fig = plt.figure(1)
-bx=fig.add_subplot(111)
-bx.plot(vecs[:,0], c="crimson")
-bx.plot(vecs[:,1], c="coral")
-
-fig = plt.figure(2)
-cx=fig.add_subplot(111)
-cx.plot(eigw, c="crimson")
-
-fig = plt.figure(3)
-dx=fig.add_subplot(111)
-dx.scatter(np.arange(0,np.size(eigw),1),eigw, c="crimson", marker=".")
-dx.set_xlim(0,15)
-dx.set_ylim(0,0.11)
-dx.set_title("")
-
+#%%
 # plot guessed 
-fig = plt.figure(4)
+fig = plt.figure(1)
 ax=fig.add_subplot(111,projection='3d')
-ax.scatter(_guess.x, _guess.y, linewidth= 1, c='k', marker='.')
+ax.scatter(gcoord.x, gcoord.y, linewidth= 1, c='coral', marker='*')
+ax.scatter(tcoord.x, tcoord.y, linewidth= 1, c = 'dodgerblue', marker='.' )
 ax.set_title("")
+ax.legend(("guessed", "ideal"))
 
-f = plt.figure(5)
+f = plt.figure(2)
 ex = f.add_subplot(111,projection='3d')
-ex.scatter(gc.x, gc.y, linewidth= 1, c='k', marker='*')
+ex.scatter(gcc.x, gcc.y, linewidth= 1, c = 'crimson', marker='*')
+ex.scatter(tcoord.x, tcoord.y, linewidth= 1, c = 'dodgerblue', marker='.' )
+ex.legend(("after mass tuning", "ideal"))
+
+figs = plt.figure(3)
+fx = figs.add_subplot(111,projection='3d')
+
+fx.scatter(gcc.x, gcc.y, linewidth= 1, c = 'crimson', marker='*')
+fx.scatter(tcoord.x, tcoord.y, linewidth= 1, c = 'dodgerblue', marker='.' )
+fx.scatter(gcoord.x, gcoord.y, linewidth= 1, c = 'coral', marker='*')
+fx.legend(( "after mass tuning","ideal", "guessed"))
+#%%
+ff = plt.figure(4)
+bx = ff.add_subplot(111,projection='3d')
+bx.scatter(gcc.x, gcc.y, c = masses, s = masses*100, marker = 'o')
+
+
+
+#%%
+import seaborn as sns
+mx = np.split(masses,N)
+mat = np.asmatrix(mx)
+f, ax = plt.subplots(figsize=(9, 6))
+sns.heatmap(mat, annot=True, linewidths=.5, ax=ax)
+
+#%%
 
 
 
 
+eigwals, eigvecs = LA.eigh(nuovolaplaciano)
 
-
-
-
-
-
-
-
-
-
+fig = plt.figure(5)
+cx=fig.add_subplot(111)
+cx.scatter(np.arange(0,np.size(eigw),1), eigw, c="aquamarine", marker = ".", s = 18**2, alpha = 0.7)
+cx.scatter(np.arange(0,np.size(eigwals),1), eigwals, c="crimson", marker = ".", s = 18**2, alpha = 0.7)
+cx.set_xlim(0,15)
+cx.set_ylim(0,0.11)
+cx.legend(("original", "mass tuned"))
 
 
 
